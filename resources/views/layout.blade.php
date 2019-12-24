e63a191a9230f0e8eb953aca0d904901553f079a
riquemath <fredabisai@gmail.com>
2019-12-24 08:30:09 +0300

    Make a layout blade file

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+ 
+     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+     <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+      <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+     <title>
+       @yield('page_title')
+    </title>
+  </head>
+  <body style="background: #f0f2fc;">
+     <div id="app" style="box-sizing: border-box;" >
+     
+
+      @yield('content')
+
+    </div>
+
+  
+
+  </body>
+</html>
