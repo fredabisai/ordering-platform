a074b7450ff4f4bb9f86e8ab88673a6c6d1ab354
riquemath <fredabisai@gmail.com>
2019-12-24 08:56:59 +0300

    Add routes for login

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', 'PagesController@login');
+Route::post('/login', 'LoginController@authenticate')->name('login');
