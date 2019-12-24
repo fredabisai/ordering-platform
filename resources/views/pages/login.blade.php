062089e96eebd23a749cb206efdba7756b79cb67
riquemath <fredabisai@gmail.com>
2019-12-24 13:06:44 +0300

    new changes

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -45,13 +45,7 @@ Login
 
                         <div class="form-group row">
                             <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
+                                
                             </div>
                         </div>
 
